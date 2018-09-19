68f218ecaedf01394e52dfeada38da9709f6e25f
JimenezR47 <43394688+JimenezR47@users.noreply.github.com>
2018-09-18 22:16:54 -0400

    Add files via upload

---
+++ LetterToSelf/LetterToSelf/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetterToSelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // LetterToSelf     Purpose: To display my name and address as if it were a letter.     Date: 9/11/2018
+
+            Console.WriteLine("+----------------------------------------------------------------+");
+            Console.WriteLine("|                                                           #### |");
+            Console.WriteLine("|                                                           #### |");
+            Console.WriteLine("|                                                           #### |");
+            Console.WriteLine("|                                                                |");
+            Console.WriteLine("|                                Rigoberto Jimenez               |");
+            Console.WriteLine("|                                2701 Spring Street              |");
+            Console.WriteLine("|                                Fort Wayne, IN 46808            |");
+            Console.WriteLine("|                                                                |");
+            Console.WriteLine("|                                                                |");
+            Console.WriteLine("+----------------------------------------------------------------+");
+            Console.ReadKey();
+        }
+    }
+}
