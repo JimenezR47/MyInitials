8425709e17b773d0a3721d7ec395440371058947
JimenezR47 <43394688+JimenezR47@users.noreply.github.com>
2018-09-18 22:12:13 -0400

    Add files via upload

---
+++ MyInitials/MyInitials/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyInitials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // MyInitials       Purpose: To Display personal initials in block letters.     Date: 9/11/2018
+
+            Console.WriteLine("For the name Rigoberto Jimenez...");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("RRRR   JJJJJ");
+            Console.WriteLine("R   R    J  ");
+            Console.WriteLine("R   R    J  ");
+            Console.WriteLine("RRRR     J  ");
+            Console.WriteLine("R R    J J  ");
+            Console.WriteLine("R  R   J J  ");
+            Console.WriteLine("R   R   JJ  ");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
