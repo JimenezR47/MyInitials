d85a46c7977ae61715864bf59139890d5f604bc5
JimenezR47 <43394688+JimenezR47@users.noreply.github.com>
2018-12-06 19:19:37 -0500

    Add files via upload

---
+++ SwapListBoxValues/SwapListBoxValues/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace SwapListBoxValues
+{
+    partial class SwapBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbGenerate50 = new System.Windows.Forms.ListBox();
+            this.lbGenerate10 = new System.Windows.Forms.ListBox();
+            this.bGenerate50 = new System.Windows.Forms.Button();
+            this.bSwap = new System.Windows.Forms.Button();
+            this.bGenerate10 = new System.Windows.Forms.Button();
+            this.bSave50 = new System.Windows.Forms.Button();
+            this.bLoad50 = new System.Windows.Forms.Button();
+            this.bClear = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.bSave10 = new System.Windows.Forms.Button();
+            this.bLoad10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbGenerate50
+            // 
+            this.lbGenerate50.FormattingEnabled = true;
+            this.lbGenerate50.Location = new System.Drawing.Point(12, 12);
+            this.lbGenerate50.Name = "lbGenerate50";
+            this.lbGenerate50.Size = new System.Drawing.Size(120, 420);
+            this.lbGenerate50.TabIndex = 0;
+            // 
+            // lbGenerate10
+            // 
+            this.lbGenerate10.FormattingEnabled = true;
+            this.lbGenerate10.Location = new System.Drawing.Point(219, 12);
+            this.lbGenerate10.Name = "lbGenerate10";
+            this.lbGenerate10.Size = new System.Drawing.Size(120, 420);
+            this.lbGenerate10.TabIndex = 1;
+            // 
+            // bGenerate50
+            // 
+            this.bGenerate50.Location = new System.Drawing.Point(138, 12);
+            this.bGenerate50.Name = "bGenerate50";
+            this.bGenerate50.Size = new System.Drawing.Size(75, 23);
+            this.bGenerate50.TabIndex = 2;
+            this.bGenerate50.Text = "Generate 50";
+            this.bGenerate50.UseVisualStyleBackColor = true;
+            this.bGenerate50.Click += new System.EventHandler(this.bGenerate50_Click);
+            // 
+            // bSwap
+            // 
+            this.bSwap.Enabled = false;
+            this.bSwap.Location = new System.Drawing.Point(138, 70);
+            this.bSwap.Name = "bSwap";
+            this.bSwap.Size = new System.Drawing.Size(75, 23);
+            this.bSwap.TabIndex = 3;
+            this.bSwap.Text = "Swap";
+            this.bSwap.UseVisualStyleBackColor = true;
+            this.bSwap.Click += new System.EventHandler(this.bSwap_Click);
+            // 
+            // bGenerate10
+            // 
+            this.bGenerate10.Enabled = false;
+            this.bGenerate10.Location = new System.Drawing.Point(138, 41);
+            this.bGenerate10.Name = "bGenerate10";
+            this.bGenerate10.Size = new System.Drawing.Size(75, 23);
+            this.bGenerate10.TabIndex = 4;
+            this.bGenerate10.Text = "Generate 10";
+            this.bGenerate10.UseVisualStyleBackColor = true;
+            this.bGenerate10.Click += new System.EventHandler(this.bGenerate10_Click);
+            // 
+            // bSave50
+            // 
+            this.bSave50.Enabled = false;
+            this.bSave50.Location = new System.Drawing.Point(138, 119);
+            this.bSave50.Name = "bSave50";
+            this.bSave50.Size = new System.Drawing.Size(75, 23);
+            this.bSave50.TabIndex = 5;
+            this.bSave50.Text = "Save 50";
+            this.bSave50.UseVisualStyleBackColor = true;
+            this.bSave50.Click += new System.EventHandler(this.bSave50_Click);
+            // 
+            // bLoad50
+            // 
+            this.bLoad50.Enabled = false;
+            this.bLoad50.Location = new System.Drawing.Point(138, 201);
+            this.bLoad50.Name = "bLoad50";
+            this.bLoad50.Size = new System.Drawing.Size(75, 23);
+            this.bLoad50.TabIndex = 6;
+            this.bLoad50.Text = "Load 50";
+            this.bLoad50.UseVisualStyleBackColor = true;
+            this.bLoad50.Click += new System.EventHandler(this.bLoad50_Click);
+            // 
+            // bClear
+            // 
+            this.bClear.Enabled = false;
+            this.bClear.Location = new System.Drawing.Point(138, 409);
+            this.bClear.Name = "bClear";
+            this.bClear.Size = new System.Drawing.Size(75, 23);
+            this.bClear.TabIndex = 7;
+            this.bClear.Text = "Clear";
+            this.bClear.UseVisualStyleBackColor = true;
+            this.bClear.Click += new System.EventHandler(this.bClear_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // bSave10
+            // 
+            this.bSave10.Enabled = false;
+            this.bSave10.Location = new System.Drawing.Point(138, 148);
+            this.bSave10.Name = "bSave10";
+            this.bSave10.Size = new System.Drawing.Size(75, 23);
+            this.bSave10.TabIndex = 8;
+            this.bSave10.Text = "Save 10";
+            this.bSave10.UseVisualStyleBackColor = true;
+            this.bSave10.Click += new System.EventHandler(this.bSave10_Click);
+            // 
+            // bLoad10
+            // 
+            this.bLoad10.Enabled = false;
+            this.bLoad10.Location = new System.Drawing.Point(138, 230);
+            this.bLoad10.Name = "bLoad10";
+            this.bLoad10.Size = new System.Drawing.Size(75, 23);
+            this.bLoad10.TabIndex = 9;
+            this.bLoad10.Text = "Load 10";
+            this.bLoad10.UseVisualStyleBackColor = true;
+            this.bLoad10.Click += new System.EventHandler(this.bLoad10_Click);
+            // 
+            // SwapBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 450);
+            this.Controls.Add(this.bLoad10);
+            this.Controls.Add(this.bSave10);
+            this.Controls.Add(this.bClear);
+            this.Controls.Add(this.bLoad50);
+            this.Controls.Add(this.bSave50);
+            this.Controls.Add(this.bGenerate10);
+            this.Controls.Add(this.bSwap);
+            this.Controls.Add(this.bGenerate50);
+            this.Controls.Add(this.lbGenerate10);
+            this.Controls.Add(this.lbGenerate50);
+            this.Name = "SwapBox";
+            this.Text = "Swap Box";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbGenerate50;
+        private System.Windows.Forms.ListBox lbGenerate10;
+        private System.Windows.Forms.Button bGenerate50;
+        private System.Windows.Forms.Button bSwap;
+        private System.Windows.Forms.Button bGenerate10;
+        private System.Windows.Forms.Button bSave50;
+        private System.Windows.Forms.Button bLoad50;
+        private System.Windows.Forms.Button bClear;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.Button bSave10;
+        private System.Windows.Forms.Button bLoad10;
+    }
+}
+
---
+++ SwapListBoxValues/SwapListBoxValues/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+// Author: Rigoberto Jimenez    Purpose: To generate a list of 50 numbers and a list of 10 numbers than to swap them. Also the ability to save to a file and import.    Date: 10/28/2018
+namespace SwapListBoxValues
+{
+    public partial class SwapBox : Form
+    {
+        public bool Value { get; set; }
+        public SwapBox()
+        {
+            InitializeComponent();
+        }
+
+        private void bGenerate50_Click(object sender, EventArgs e)
+        {
+            lbGenerate50.Items.Clear();
+
+            Random randomNumber = new Random();
+            int tempNumber;
+
+            for (int i = 1; i <= 50; i++)
+            {
+                tempNumber = randomNumber.Next(1000);
+                lbGenerate50.Items.Add(tempNumber);
+            }
+
+            bGenerate10.Enabled = true;
+        }
+
+        private void bGenerate10_Click(object sender, EventArgs e)
+        {
+            lbGenerate10.Items.Clear();
+
+            Random randomNumber = new Random();
+            int tempNumber;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                tempNumber = randomNumber.Next(100);
+                lbGenerate10.Items.Add(tempNumber);
+            }
+            bSave50.Enabled = true;
+            bSwap.Enabled = true;
+        }
+
+        private void bSave50_Click(object sender, EventArgs e)
+        {
+            StreamWriter outputFile;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                outputFile = File.CreateText(saveFileDialog.FileName);
+
+                for (int i = 0; i < lbGenerate50.Items.Count; i++)
+                {
+                    outputFile.WriteLine(lbGenerate50.Items[i]);
+                }
+
+                outputFile.Close();
+
+                MessageBox.Show("Numbers 50 saved.");
+                bSave10.Enabled = true;
+            }
+        }
+
+        private void bSave10_Click(object sender, EventArgs e)
+        {
+            StreamWriter outputFile;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                outputFile = File.CreateText(saveFileDialog.FileName);
+
+                for (int i = 0; i < lbGenerate10.Items.Count; i++)
+                {
+                    outputFile.WriteLine(lbGenerate10.Items[i]);
+                }
+
+                outputFile.Close();
+
+                MessageBox.Show("Numbers 10 saved.");
+                bClear.Enabled = true;
+            }
+        }
+
+        private void bClear_Click(object sender, EventArgs e)
+        {
+            lbGenerate50.Items.Clear();
+            lbGenerate10.Items.Clear();
+
+            bLoad50.Enabled = true;
+        }
+
+        private void bLoad50_Click(object sender, EventArgs e)
+        {
+            StreamReader inputFile;
+            string lineIn;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                inputFile = File.OpenText(openFileDialog.FileName);
+
+                while (!inputFile.EndOfStream)
+                {
+                    lineIn = inputFile.ReadLine();
+
+                    lbGenerate50.Items.Add(lineIn);
+                }
+
+                inputFile.Close();
+                MessageBox.Show("Numbers 50 loaded.");
+                bLoad10.Enabled = true;
+            }
+        }
+
+        private void bLoad10_Click(object sender, EventArgs e)
+        {
+            StreamReader inputFile;
+            string lineIn;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                inputFile = File.OpenText(openFileDialog.FileName);
+
+                while (!inputFile.EndOfStream)
+                {
+                    lineIn = inputFile.ReadLine();
+
+                    lbGenerate10.Items.Add(lineIn);
+                }
+
+                inputFile.Close();
+                MessageBox.Show("Numbers 10 loaded.");
+            }
+        }
+
+        private void bSwap_Click(object sender, EventArgs e)
+        {
+            if (Value == true)
+            {
+                lbGenerate10.Location = new Point(12, 12);
+                lbGenerate50.Location = new Point(219, 12);
+
+                Value = false;
+            }
+
+            else
+            {
+                lbGenerate10.Location = new Point(219, 12);
+                lbGenerate50.Location = new Point(12, 12);
+
+                Value = true;
+            }
+        }
+    }
+}
